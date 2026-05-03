e392f602181e463ea17bbba06a6b7cf0111a988a
Yurii Kravchenko <yuriy@hubmee.com>
2026-05-03 21:07:33 +0300

    fix: update lesson-4 django deployment config.

--- lesson-4/django/project/settings.py
+++ lesson-4/django/project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8ky+95@e9tj9u2r()z=b9fjf4qav#v8#l7sl&lccqg0_msp=ci'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
